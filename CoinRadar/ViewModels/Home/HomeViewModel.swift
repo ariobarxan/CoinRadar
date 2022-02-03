d6ab3c60b39a6aac21037f96eeedcb413837b3ed
Azhman <bapir.dev@gmail.com>
2022-02-03 21:15:57 +0330

    Filtering from homeView SearchField is implemented

--- CoinRadar/ViewModels/Home/HomeViewModel.swift
+++ CoinRadar/ViewModels/Home/HomeViewModel.swift
@@ -20,11 +20,38 @@ final class HomeViewModel: ObservableObject{
     }
 
     private func addSubscribers(){
-        APIService.$coins
+        //APIService
+        /*
+         APIService.$coins
             .sink { [weak self] coins in
                 guard let self = self else {return}
                 self.coins = coins
             }
             .store(in: &cancellables)
+         */
+        
+        //Search String and APIService combined
+        $searchString
+            .combineLatest(APIService.$coins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filteredCoins)
+            .sink { [weak self] filteredCoins in
+                self?.coins = filteredCoins
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func filteredCoins(text searchString: String, coins: [Coin]) -> [Coin] {
+        guard !searchString.isEmpty else {
+            return coins
+            
+        }
+        let lowerdCaseSearchString = searchString.lowercased()
+        
+        return coins.filter { coin -> Bool in
+            return coin.name.lowercased().contains(lowerdCaseSearchString) ||
+            coin.symbol.lowercased().contains(lowerdCaseSearchString) ||
+            coin.id.lowercased().contains(lowerdCaseSearchString)
+        }
     }
 }
